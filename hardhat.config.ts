4d7dd1fd0f97e3483c4b3ceddf71aa806b494460
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2022-11-29 01:12:45 +0300

    feat: first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,54 @@
+import { task, HardhatUserConfig } from 'hardhat/config';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+
+import deployer from './.secret';
+
+// const KCC_RPC = 'https://127.0.0.1:8545/';
+const avalancheMainnet = 'https://api.avax.network/ext/bc/C/rpc';
+const avalancheFuji = 'https://api.avax-test.network/ext/bc/C/rpc';
+const avalancheFork = 'https://avalanche-mainnet.infura.io/v3/a11908c0f4dc461381e780261e681154';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.7.6',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      loggingEnabled: true,
+      forking: {
+        url: avalancheFork,
+        enabled: true,
+      },
+      accounts: {
+        accountsBalance: '1000000000000000000000000', // 1 mil ether
+      },
+    },
+    fuji: {
+      url: avalancheFuji,
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [deployer.private],
+    },
+    mainnet: {
+      url: avalancheMainnet,
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [deployer.private],
+    },
+  },
+  // mocha: {
+  //   timeout: 40000,
+  // },
+};
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers, run } from 'hardhat';
+
+import deployer from '../.secret';
+
+// WKCS address on KCC, WETH address on ETH
+const wavaxAddr = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7';
+
+async function main() {
+  await run('compile');
+  const FlashBot = await ethers.getContractFactory('FlashBot');
+  const flashBot = await FlashBot.deploy(wavaxAddr);
+
+  console.log(`FlashBot deployed to ${flashBot.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
